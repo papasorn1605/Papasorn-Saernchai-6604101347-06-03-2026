88637f805d7cae408466cf7bfe63d4165b971d9e
papasorn1605 <papasorn1605@gmail.com>
2026-03-06 13:05:02 +0700

    chore: update database config for deploy

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     schema: 'prisma/schema.prisma',
     datasource: {
         provider: 'postgresql',
-        url: process.env.DATABASE_URL
+        url: process.env.DIRECT_URL || process.env.DATABASE_URL
     },
     migrations: {
         seed: 'node prisma/seed.js'
